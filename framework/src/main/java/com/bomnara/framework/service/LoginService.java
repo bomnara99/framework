eaac5e5f6666087602fe229db422de817a3f1e1c
bomnara99 <bibi0814@naver.com>
2025-04-07 11:52:24 +0900

    Delete framework directory

--- framework/src/main/java/com/bomnara/framework/service/LoginService.java
+++
@@ -1,6 +0,0 @@
-package com.bomnara.framework.service;
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface LoginService {
-	UserDetails loadUserByUsername(String username) throws Exception ;
-}
